c806c53258bdaf701cf7584cda60e9527df8d037
jattoabdul <jattoade@gmail.com>
2020-01-25 10:03:19 +0100

    add country to serializable

--- app/serializable/serializable_product.rb
+++ app/serializable/serializable_product.rb
@@ -1,7 +1,7 @@
 class SerializableProduct < JSONAPI::Serializable::Resource
   type :product
   attribute(:id) { @object.id }
-  attributes :title, :description, :price, :tags
+  attributes :title, :description, :price, :tags, :country
 
   attribute(:created_at) { @object.created_at&.utc }
   attribute(:updated_at) { @object.updated_at&.utc }
