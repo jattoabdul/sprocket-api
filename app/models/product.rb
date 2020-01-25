074b3d22f4ae2fcc28ab7f40e8febf61c31510e2
jattoabdul <jattoade@gmail.com>
2020-01-25 12:55:19 +0100

    Adding more to search data & caching on commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,22 +5,23 @@ class Product < ApplicationRecord
   # searchkick
   searchkick text_middle: %i(title description country price), callbacks: :async
 
-  # after_commit :flush_cache!
+  after_commit :flush_cache!
   
   def search_data
     {
       title: title,
       description: description,
       country: country,
-      price: price,
-      tags: tags
+      price: price.to_f,
+      tags: tags,
+      updated_at: updated_at
     }
   end
 
   private
 
-  # def flush_cache!
-  #   # Clear products:* caches
-  #   Rails.cache.delete_matched 'products:*'
-  # end
+  def flush_cache!
+    # Clear products:* caches
+    Rails.cache.delete_matched 'products:*'
+  end
 end
